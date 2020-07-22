f669f187c34f09266f476c22e91f177951ecd19b
omnihound <omnihound@gmail.com>
2020-07-23 07:50:31 +1000

    Updates

---
+++ precache-manifest.0bb665f5e0f9a5b13ff24cac391703ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d8d5b7c2360b28906a035693d7d60ea",
+    "url": "/pulse-tracker/index.html"
+  },
+  {
+    "revision": "4a006e1a61893a7b815c",
+    "url": "/pulse-tracker/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "721051eb56442721e83e",
+    "url": "/pulse-tracker/static/js/2.798e8748.chunk.js"
+  },
+  {
+    "revision": "5bd09eb7f2cf4d145d532d862be18942",
+    "url": "/pulse-tracker/static/js/2.798e8748.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a006e1a61893a7b815c",
+    "url": "/pulse-tracker/static/js/main.cbe002b9.chunk.js"
+  },
+  {
+    "revision": "da4e0ecdf812dad3259c",
+    "url": "/pulse-tracker/static/js/runtime-main.0a502dc4.js"
+  }
+]);
